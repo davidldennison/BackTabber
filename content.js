324b3bca9784fb80dcac39835c3ca1136fe88278
David Dennison <david@daviddennison.com>
2026-04-17 04:46:44 -0700

    Delete content.js
    Oopsy

--- content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', function(event) {
-  const link = event.target.closest('a');
-  if (link && link.href && link.href.startsWith('http')) {
-    event.preventDefault();
-    chrome.runtime.sendMessage({ url: link.href });
-  }
-}, false);
